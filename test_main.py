f7896b36e9d340c8149700f5f4cec29fe18dd1b9
Sourav Malani <i190434@nu.edu.pk>
2023-09-11 19:12:45 +0500

    Final Try

--- test_main.py
+++ test_main.py
@@ -1,13 +1,11 @@
 import unittest
 from main import add
 
-w = wallet
-
 class TestWallet(unittest.TestCase):
 
     def test_add(self):  # Add `self` as the first parameter
         result = add(2, 3)
-        assert result == 3
+        assert result == 5
 
 if __name__ == "__main__":
     unittest.main()
