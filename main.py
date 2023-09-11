2303006bddd2dcfbae6fbe5e13015e41fe3f48d3
Sourav Malani <i190434@nu.edu.pk>
2023-09-11 12:11:49 +0500

    added requirments

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+#Create wallet class with the following methods: getBalance, addBalance, removeBalance
+class wallet:
+    balance = 0
+    def __init__(self, balance):
+        self.balance = balance
+    def getBalance(self):
+        return self.balance
+    def addBalance(self, amount):
+        self.balance += amount
+    def removeBalance(self, amount):
+        self.balance -= amount
+
 def add(a, b):
     return a + b
 
 if __name__ == "__main__":
     result = add(2, 3)
-    print(f"The result is: {result}")
+    print(f"The result is: {result}")    
+#add funtion to take power of a number
+
--- test_main.py
+++ test_main.py
@@ -6,5 +6,10 @@ class TestAddition(unittest.TestCase):
         result = add(2, 3)
         self.assertEqual(result, 5)
 
+class TestAddition2(unittest.TestCase):
+    def test_addition(self):
+        result = add(2, 4)
+        self.assertEqual(result, 6)
+
 if __name__ == "__main__":
     unittest.main()
