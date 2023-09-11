218454ee00db3c765e821377ba84ca2179456022
Sourav Malani <i190434@nu.edu.pk>
2023-09-11 12:37:10 +0500

    updated main file

--- test_main.py
+++ test_main.py
@@ -1,12 +1,12 @@
 import unittest
 from main import wallet
 
-
 w = wallet
+
 class TestWallet(unittest.TestCase):
-    def test_add_balance():
+    def test_add_balance(self):  # Add `self` as the first parameter
         result = w.addBalance(3)
-        assert   w.getBalance() == 3
+        assert w.getBalance() == 3
 
 if __name__ == "__main__":
     unittest.main()
