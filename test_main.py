67411dfba225bf341faa391ad5b5248fd5de1b87
Sourav Malani <i190434@nu.edu.pk>
2023-09-11 12:41:23 +0500

    updated main file

--- test_main.py
+++ test_main.py
@@ -4,8 +4,9 @@ from main import wallet
 w = wallet
 
 class TestWallet(unittest.TestCase):
+
     def test_add_balance(self):  # Add `self` as the first parameter
-        result = w.addBalance(3)
+        result = w.addBalance(2, 3)
         assert w.getBalance() == 3
 
 if __name__ == "__main__":
