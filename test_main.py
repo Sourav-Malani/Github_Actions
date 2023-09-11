b38fa6a4baf5e6e3a032679e038c827966331f66
Sourav Malani <i190434@nu.edu.pk>
2023-09-11 12:28:04 +0500

    updated test file

--- test_main.py
+++ test_main.py
@@ -1,16 +1,12 @@
 import unittest
 from main import wallet
 
-# class test_TestAddition(unittest.TestCase):
-#     def test_addition(self):
-#         result = add(2, 3)
-#         self.assertEqual(result, 5)
 
-#create class intsance
 w = wallet
-def test_add_balance():
-    result = wallet.addBalance(w, 3)
-    assert wallet.getBalance() == 3
+class TestWallet(unittest.TestCase):
+    def test_add_balance():
+        result = wallet.addBalance(w, 3)
+        assert wallet.getBalance() == 3
 
 if __name__ == "__main__":
     unittest.main()
